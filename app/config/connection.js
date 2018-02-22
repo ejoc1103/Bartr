4b6400a269195bf9648b2de1ef588dc94b7017f3
Sean Dillon <seandillon1224@gmail.com>
2018-02-22 11:55:12 -0500

    initialcommit

---
+++ app/config/connection.js
@@ -0,0 +1,23 @@
+var mysql = require("mysql");
+
+// mysql://b0eee3b726d6bc:d470811d@us-cdbr-iron-east-05.cleardb.net/heroku_d838a86c021266c?reconnect=true
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "bartr_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ app/server.js
@@ -0,0 +1,45 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json()); 
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Routes
+// ================================================== ===========
+require("./controllers/api-routes.js")(app);
+require("./controllers/html-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
