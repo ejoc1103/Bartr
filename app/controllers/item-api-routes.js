3f93eca19cee3643d06b7d5c7c5a8107b396a107
Terry <trrychng@gmail.com>
2018-02-28 23:11:29 -0500

    clean up

--- app/controllers/item-api-routes.js
+++ app/controllers/item-api-routes.js
@@ -49,7 +49,7 @@ module.exports = function (app) {
                 CategoryId: req.body.inputCategory
 
             }).then(function (results) {
-                res.redirect("/")
+                res.redirect("/items")
             });
 
         });
