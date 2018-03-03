eb5e6db0eb9610b35f03d2fb1bc162c9699b62ef
Sean Dillon <seandillon1224@gmail.com>
2018-03-02 23:34:11 -0500

    newadds

--- app/controllers/edit.js
+++ app/controllers/edit.js
@@ -40,6 +40,63 @@ app.put("/api/items", function(req,res){
 
 });
 
+app.get("/editprofile", function (req, res) {
+    db.User.find({
+        where: {
+            userName: req.session.passport.user.userName
+        }
+    }).then(function (results) {
+        let userItems = {
+            userItems: results
+        }
+        res.render("editprofile", {
+            userItems: userItems
+        })
+    })
+})
+
+app.post("/editprofile", function (req, res) {
+    console.log(req.body)
+            var fileThing = ""
+            // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+            if (req.files.uploaded_newprofile) {
+
+                let sampleFile = req.files.uploaded_newprofile;
+
+                fileThing = '/assets/img/profile/' + req.body.username + sampleFile.name
+
+                // Use the mv() method to place the file somewhere on your server
+                sampleFile.mv('./public/assets/img/profile/' + req.body.username + sampleFile.name)
+            } else {
+
+                fileThing = req.body.imgSource
+            }
+                db.User.update({
+                    userName: req.body.username,
+                    email: req.body.email,
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    imgSource: fileThing},
+                    {where: {userName: req.session.passport.user.userName}
+                }).then(function (results) {
+                    res.render("profile")
+
+                })
+                });
+            
+        }
+
+            function authenticationMiddleware() {
+                return (req, res, next) => {
+                    // console.log(`
+                    // req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
+                    if (req.isAuthenticated()) return next();
+
+                    res.redirect("/login")
+                }
+            }
+
+
 // app.delete("/api/items/:id"), function(req, res){
 //     console.log(inputted + "hey this inputted!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
   
@@ -52,7 +109,8 @@ app.put("/api/items", function(req,res){
 //             res.redirect("/items")
 //         })
 //   }
-  }
+
+
 
 
 
--- app/controllers/offer-routes.js
+++ app/controllers/offer-routes.js
@@ -3,42 +3,41 @@ var db = require("../models");
 module.exports = function (app) {
 
     app.post("/api/offers/:itemId", function (req, res) {
-            var fileThing = ""
-            console.log(req.files)
-        // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-        // if (req.files.uploaded_offer) {
-        //     let sampleFile = req.files.uploaded_offer;
+        var fileThing = ""
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    if (req.files.uploaded_offer) {
+        let sampleFile = req.files.uploaded_offer;
 
-        //     fileThing = '/assets/img/offers/' + req.body.username + sampleFile.name
+        fileThing = '/assets/img/offers/' + req.body.username + sampleFile.name
 
-        //     // Use the mv() method to place the file somewhere on your server
-        //     sampleFile.mv('./public/assets/img/offers/' + req.body.username + sampleFile.name)
-        // } else {
-        //     fileThing ='/assets/img/offers/defaultoffer.png'
-        // }
+        // Use the mv() method to place the file somewhere on your server
+        sampleFile.mv('./public/assets/img/offers/' + req.body.username + sampleFile.name)
+    } else {
+        res.render('/items')
+    }
 
-        
-        let itemId = req.params.itemId
+    
+    let itemId = req.params.itemId
 
 
-        db.User.find({
+    db.User.find({
 
-            where: {
-                userName: req.session.passport.user.userName
-            }
+        where: {
+            userName: req.session.passport.user.userName
+        }
+    }).then(function (results) {
+        var currentUserId = results.id
+
+        db.Offers.create({
+            userOffer: req.body.offerDescription,
+            ItemId: itemId,
+            UserId: currentUserId,
+            imgSource: fileThing
+
         }).then(function (results) {
-            var currentUserId = results.id
-
-            db.Offers.create({
-                userOffer: req.body.offerDescription,
-                ItemId: itemId,
-                UserId: currentUserId,
-                imgSource: fileThing
-
-            }).then(function (results) {
-                res.redirect("/profile")
-            });
-
+            res.redirect("/profile")
         });
+
     });
+});
 }
