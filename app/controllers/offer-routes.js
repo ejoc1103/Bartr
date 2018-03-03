838bfcacf5b4ed72240aa0f9eda3d39ac72e1178
Terry <trrychng@gmail.com>
2018-03-02 23:16:32 -0500

    comment out offer img

--- app/controllers/offer-routes.js
+++ app/controllers/offer-routes.js
@@ -4,17 +4,18 @@ module.exports = function (app) {
 
     app.post("/api/offers/:itemId", function (req, res) {
             var fileThing = ""
+            console.log(req.files)
         // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-        if (req.files.uploaded_offer) {
-            let sampleFile = req.files.uploaded_offer;
+        // if (req.files.uploaded_offer) {
+        //     let sampleFile = req.files.uploaded_offer;
 
-            fileThing = '/assets/img/offers/' + req.body.username + sampleFile.name
+        //     fileThing = '/assets/img/offers/' + req.body.username + sampleFile.name
 
-            // Use the mv() method to place the file somewhere on your server
-            sampleFile.mv('./public/assets/img/offers/' + req.body.username + sampleFile.name)
-        } else {
-            res.render('/items')
-        }
+        //     // Use the mv() method to place the file somewhere on your server
+        //     sampleFile.mv('./public/assets/img/offers/' + req.body.username + sampleFile.name)
+        // } else {
+        //     fileThing ='/assets/img/offers/defaultoffer.png'
+        // }
 
         
         let itemId = req.params.itemId
